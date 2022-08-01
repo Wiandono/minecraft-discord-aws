cca763dec86270d96699e124207c89137a04d26e
xylomas_etta <xylomas_etta@slmail.me>
2022-08-02 00:35:54 +0900

    fix(handler): add working slash command
    The action is still dummy tho

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
+import base64
 import json
+import os
+
+from discord_interactions import verify_key, InteractionType, InteractionResponseType
+
+
+def verify_signature(event, decoded_body: bytes) -> bool:
+    signature = event['headers']['x-signature-ed25519']
+    timestamp = event['headers']['x-signature-timestamp']
+
+    return verify_key(decoded_body, signature, timestamp, os.getenv('BOT_PUBLIC_KEY'))
 
 
 def function(event, context):
+    print(event)
+
+    if 'python-requests' in event['headers']['User-Agent']:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Health check from zappa deployment')
+        }
+
+    decoded_body = base64.b64decode(event['body'])
+
+    if not verify_signature(event, decoded_body):
+        return {
+            'statusCode': 401,
+            'body': json.dumps('Signature is invalid')
+        }
+
+    dict_body = json.loads(decoded_body)
+
+    if dict_body['type'] == InteractionType.PING:
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'type': InteractionResponseType.PONG}),
+        }
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({
+            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            'data': {
+                'tts': False,
+                'content': 'Bangun zar',
+                'embeds': [],
+                'allowed_mentions': [],
+            }
+        }),
     }
